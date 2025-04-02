3228fee8fb1335cd1a8d9ee26af335c6cff6dbb5
Geny Vargas <gmvargass@eafit.edu.co>
2025-04-01 22:32:59 -0500

    Se realizan los cambios en la página principal

--- app/components/organisms/header-general/header-general.tsx
+++ app/components/organisms/header-general/header-general.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Button from "~/components/atoms/button/button";
+import Button from '../../atoms/button/button';
 import Logo from "../../../assets/images/logo-fundacion-antivirus.png";
 import IconSearch from "../../../assets/icons/icon-search.png";
 import IconSun from "../../../assets/icons/icon-sun.png";
@@ -10,6 +10,7 @@ import { useNavigate, Link } from "react-router-dom";
 export default function HeaderGeneral() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [darkMode, setDarkMode] = useState(false);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const handlelogin = () => {
@@ -30,7 +31,6 @@ export default function HeaderGeneral() {
       ${darkMode ? "bg-gray-900 text-white" : "bg-white text-black"}
     `}
     >
-      {/* Logo y menú de navegación */}
       <div className="flex items-center w-full md:w-auto">
         <img
           src={Logo}
@@ -38,125 +38,54 @@ export default function HeaderGeneral() {
           className="w-[50px] h-[50px] object-contain"
         />
 
-        {/* Menú en pantallas grandes */}
         <ul className="hidden xl:flex gap-6 ml-6">
-          <li>
-            <Link to="#!" className="hover:text-gray-500">
+          <li className="relative">
+            <button
+              onClick={() => setIsSubMenuOpen(!isSubMenuOpen)}
+              className="hover:text-gray-500"
+            >
               ¿Quiénes somos?
-            </Link>
+            </button>
+            {isSubMenuOpen && (
+              <div className="absolute left-0 mt-2 bg-white border rounded shadow-lg w-48">
+                <Link to="#quienes_somos" className="block px-4 py-2 hover:bg-gray-200" onClick={() => setIsSubMenuOpen(false)}>
+                  ¿Quiénes somos?
+                </Link>
+                <Link to="#our-team" className="block px-4 py-2 hover:bg-gray-200" onClick={() => setIsSubMenuOpen(false)}>
+                  Nuestro Equipo
+                </Link>
+              </div>
+            )}
           </li>
-          <li>
-            <Link to="#institutions" className="hover:text-gray-500">
-              Instituciones
-            </Link>
-          </li>
-          <li>
-            <Link to="#our-services" className="hover:text-gray-500">
-              Servicios
-            </Link>
-          </li>
-          <li>
-            <Link to="/opportunities" className="hover:text-gray-500">
-              Oportunidades
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact-us" className="hover:text-gray-500">
-              Contáctanos
-            </Link>
-          </li>
-          <li>
-            <Link to="/OurTeam" className="hover:text-gray-500">
-              Nuestro equipo{" "}
-            </Link>
-          </li>
+          <li><Link to="#institutions" className="hover:text-gray-500">Instituciones</Link></li>
+          <li><Link to="#our-services" className="hover:text-gray-500">Servicios</Link></li>
+          <li><Link to="/opportunities" className="hover:text-gray-500">Oportunidades</Link></li>
+          <li><Link to="/contact-us" className="hover:text-gray-500">Contáctanos</Link></li>
         </ul>
       </div>
 
-      {/* Botones y buscador */}
       <div className="hidden md:flex items-center gap-3">
-        {/* Formulario de búsqueda */}
         <form className="hidden md:flex items-center border border-gray-400 px-3 py-1 rounded-full">
-          <img
-            src={IconSearch}
-            alt="Ícono Buscar"
-            className="w-[18px] h-[18px]"
-          />
-          <input
-            type="text"
-            placeholder="Buscar"
-            className="bg-transparent outline-none pl-2 w-[120px]"
-          />
+          <img src={IconSearch} alt="Ícono Buscar" className="w-[18px] h-[18px]" />
+          <input type="text" placeholder="Buscar" className="bg-transparent outline-none pl-2 w-[120px]" />
         </form>
 
-        {/* Botones de sesión */}
-        <Button
-          onClick={handlelogin}
-          text="Iniciar sesión"
-          backgroundColor="#7C78B3"
-          textColor="white"
-        />
+        <Button onClick={handlelogin} text="Iniciar sesión" backgroundColor="#7C78B3" textColor="white" />
         <Button onClick={handleRegister} text="Registrarme" borderWidth={1.5} />
 
         <div className="flex items-center gap-2">
           <button onClick={toggleDarkMode} className="focus:outline-none">
             {darkMode ? (
-              <img src={IconSun} alt="Modo Claro" className=" cursor-pointer" />
+              <img src={IconSun} alt="Modo Claro" className="cursor-pointer" />
             ) : (
-              <img
-                src={IconMoon}
-                alt="Modo Oscuro"
-                className=" cursor-pointer"
-              />
+              <img src={IconMoon} alt="Modo Oscuro" className="cursor-pointer" />
             )}
           </button>
-
-          {/* Menú hamburguesa en tablets/móviles */}
-          <button
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="xl:hidden text-black dark:text-white focus:outline-none"
-          >
-            {isMenuOpen ? (
-              <X className="text-inherit" size={30} />
-            ) : (
-              <Menu className="text-inherit" size={30} />
-            )}
+          <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="xl:hidden text-black dark:text-white focus:outline-none">
+            {isMenuOpen ? <X className="text-inherit" size={30} /> : <Menu className="text-inherit" size={30} />}
           </button>
         </div>
       </div>
-
-      {/* Modo oscuro/claro */}
-
-      {/* Menú móvil/tablet */}
-      {isMenuOpen && (
-        <div
-          className={`absolute top-[80px] left-0 w-full ${
-            darkMode ? "bg-gray-800 text-white" : "bg-white text-black"
-          } shadow-md flex flex-col items-center py-4 space-y-4 xl:hidden transition-all duration-300`}
-        >
-          <a href="#!" className="hover:text-gray-400">
-            ¿Quiénes somos?
-          </a>
-          <a href="#!" className="hover:text-gray-400">
-            Instituciones
-          </a>
-          <a href="#!" className="hover:text-gray-400">
-            Servicios
-          </a>
-          <a href="#!" className="hover:text-gray-400">
-            Oportunidades
-          </a>
-          <a href="contact-us" className="hover:text-gray-400">
-            Contáctanos
-          </a>
-          <Button
-            text="Iniciar sesión"
-            backgroundColor="#7C78B3"
-            textColor="white"
-          />
-          <Button text="Registrarme" borderWidth={1.5} />
-        </div>
-      )}
     </header>
   );
 }

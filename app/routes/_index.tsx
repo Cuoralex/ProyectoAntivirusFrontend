c472ba93a32de6a07eb5dbec75792eb9775e1f65
Cuoralex <alexacuartas@gmail.com>
2025-07-05 15:55:39 -0500

    Prueba sin servicesen index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 import ContactForm from "~/components/molecules/ContactForm";
 import HomeBanner from "../assets/images/home-fundacion-antivirus.png";
 import { useEffect, useState } from "react";
@@ -7,11 +7,11 @@ import {
   InstitutionResponse,
 } from "~/services/institutions.service";
 import { institutionToCardInfoProps } from "~/utils/mappers/institutions.mappers";
-import {
-  getAllOurServices,
-  OurServiceResponse,
-} from "~/services/our-services.service";
-import { ourServiceResponseToCardInfoProps } from "~/utils/mappers/our-services.mappers";
+// import {
+//   getAllOurServices,
+//   OurServiceResponse,
+// } from "~/services/our-services.service";
+// import { ourServiceResponseToCardInfoProps } from "~/utils/mappers/our-services.mappers";
 import OurTeam from "../components/organisms/sections/ourTeam";
 import Hero from "~/components/Hero";
 
@@ -22,84 +22,84 @@ export interface CardInfoProps {
   description?: string;
 }
 
-interface SliderCardInfoProps {
-  cards: CardInfoProps[];
-}
+// interface SliderCardInfoProps {
+//   cards: CardInfoProps[];
+// }
 
-function CardInfo({ id, urlImg, title, description }: Readonly<CardInfoProps>) {
-  return (
-    <div
-      id={String(id)}
-      className="shadow-[0_4px_12px_rgba(0,0,0,0.2)] bg-[white] rounded-[10px] max-w-[450px] py-[25px] px-[35px] flex flex-col items-center text-center my-[40px] mx-[20px]"
-    >
-      <img
-        src={urlImg}
-        alt={title}
-        className="w-[250px] h-[250px] object-cover rounded-[10px]"
-      />
-      <h4 className="text-black mt-[18px] mb-[28px] font-bold text-[20px] min-h-[90px]">
-        {title}
-      </h4>
-      <p className="text-black text-center text-[18px] overflow-hidden text-ellipsis h-[130px]">
-        {description}
-      </p>
-    </div>
-  );
-}
+// function CardInfo({ id, urlImg, title, description }: Readonly<CardInfoProps>) {
+//   return (
+//     <div
+//       id={String(id)}
+//       className="shadow-[0_4px_12px_rgba(0,0,0,0.2)] bg-[white] rounded-[10px] max-w-[450px] py-[25px] px-[35px] flex flex-col items-center text-center my-[40px] mx-[20px]"
+//     >
+//       <img
+//         src={urlImg}
+//         alt={title}
+//         className="w-[250px] h-[250px] object-cover rounded-[10px]"
+//       />
+//       <h4 className="text-black mt-[18px] mb-[28px] font-bold text-[20px] min-h-[90px]">
+//         {title}
+//       </h4>
+//       <p className="text-black text-center text-[18px] overflow-hidden text-ellipsis h-[130px]">
+//         {description}
+//       </p>
+//     </div>
+//   );
+// }
 
-function SliderCardInfo({ cards }: Readonly<SliderCardInfoProps>) {
-  return (
-    <div className="w-full max-w-full mx-auto text-left">
-      {" "}
-      <Swiper
-        spaceBetween={20}
-        loop
-        breakpoints={{
-          320: {
-            slidesPerView: 1,
-          },
-          640: {
-            slidesPerView: 2,
-          },
-          1024: {
-            slidesPerView: 3,
-          },
-          1440: {
-            slidesPerView: 4,
-          },
-        }}
-      >
-        {cards.map((cardInfo: Readonly<CardInfoProps>) => (
-          <SwiperSlide key={cardInfo.id} className="flex justify-center">
-            <CardInfo {...cardInfo} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-  );
-}
+// function SliderCardInfo({ cards }: Readonly<SliderCardInfoProps>) {
+//   return (
+//     <div className="w-full max-w-full mx-auto text-left">
+//       {" "}
+//       <Swiper
+//         spaceBetween={20}
+//         loop
+//         breakpoints={{
+//           320: {
+//             slidesPerView: 1,
+//           },
+//           640: {
+//             slidesPerView: 2,
+//           },
+//           1024: {
+//             slidesPerView: 3,
+//           },
+//           1440: {
+//             slidesPerView: 4,
+//           },
+//         }}
+//       >
+//         {cards.map((cardInfo: Readonly<CardInfoProps>) => (
+//           <SwiperSlide key={cardInfo.id} className="flex justify-center">
+//             <CardInfo {...cardInfo} />
+//           </SwiperSlide>
+//         ))}
+//       </Swiper>
+//     </div>
+//   );
+// }
 
 export default function Index() {
   useEffect(() => {
     console.log("Cargando _index.tsx");
   }, []);
   const [institutions, setInstitutions] = useState<CardInfoProps[]>([]);
-  const [ourServices, setOurServices] = useState<CardInfoProps[]>([]);
+  // const [ourServices, setOurServices] = useState<CardInfoProps[]>([]);
 
   const getAllInitialInfo = async () => {
     try {
       const institutionsResponse: InstitutionResponse[] =
         await getAllInstitutions();
-      const ourServicesResponse: OurServiceResponse[] =
-        await getAllOurServices();
+      // const ourServicesResponse: OurServiceResponse[] =
+      //   await getAllOurServices();
 
       setInstitutions(institutionsResponse.map(institutionToCardInfoProps));
 
-      setOurServices(
-        ourServicesResponse
-          .map(ourServiceResponseToCardInfoProps)
-          .filter((item): item is CardInfoProps => item !== undefined)
-      );
+      // setOurServices(
+      //   ourServicesResponse
+      //     .map(ourServiceResponseToCardInfoProps)
+      //     .filter((item): item is CardInfoProps => item !== undefined)
+      // );
     } catch (error) {
       console.error("Error cargando datos:", error);
     }
@@ -162,7 +162,7 @@ export default function Index() {
           Nuestros servicios
         </h2>
         <div className="mt-[10px] text-center lg:mt-[76px]">
-          <SliderCardInfo cards={ourServices} />
+         
         </div>
       </section>
 

ae185fd7eeb930cb3708b49f7bf36a819f736878
Cuoralex <alexacuartas@gmail.com>
2025-07-08 12:41:52 -0500

    Actualiza estructura de layouts y configuración de Vercel

--- app/components/templates/layouts/auth-layout.tsx
+++ app/components/templates/layouts/auth-layout.tsx
@@ -26,7 +26,7 @@ export default function AuthLayout() {
         />
 
         <img
-          src="/assets/images/antivirus_avatar.png"
+          src="/assets/images/antivirus-avatar.png"
           alt="Antivirus Avatar"
           loading="lazy"
           className="hidden lg:block lg:max-w-[514px] h-auto"

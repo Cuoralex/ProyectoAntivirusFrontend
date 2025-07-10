623f5af18aef56fb42c91c4dd3d4034bb36b55fb
Cuoralex <alexacuartas@gmail.com>
2025-07-10 12:34:02 -0500

    Actualización de codigo register.form.tsx

--- app/store.ts
+++ app/store.ts
@@ -1,12 +1,15 @@
 import { create } from "zustand";
 
 interface AuthState {
-  userId: any;
+  userId: number | null;
+  setUserId: (id: number) => void;
   registrationSuccess: boolean;
   setRegistrationSuccess: (value: boolean) => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
+  userId: null,
+  setUserId: (id) => set({ userId: id }),
   registrationSuccess: false,
   setRegistrationSuccess: (value) => set({ registrationSuccess: value }),
 }));
